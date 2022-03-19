427e3298e33bcb6dd76950c37a99f683e5334c67
Esdras Costa <esdras.cts@outlook.com>
2022-03-19 15:35:51 -0300

    [ADD] Configuração da emissão do JWT pela controller AuthController.

--- src/services/NerdStoreEnterprise.Identidade.API/Controllers/AuthController.cs
+++ src/services/NerdStoreEnterprise.Identidade.API/Controllers/AuthController.cs
@@ -1,7 +1,17 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using NerdStoreEnterprise.Identidade.API.Models;
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using NerdStoreEnterprise.Identidade.API.Extensions;
+using NerdStoreEnterprise.Identidade.API.Models;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
+using System.Linq;
+
 
 namespace NerdStoreEnterprise.Identidade.API.Controllers
 {
@@ -11,11 +21,15 @@ namespace NerdStoreEnterprise.Identidade.API.Controllers
     {
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly AppSettings _appSettings;
 
-        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        public AuthController(SignInManager<IdentityUser> signInManager,
+                                UserManager<IdentityUser> userManager,
+                                IOptions<AppSettings> appSettings)
         {
             _signInManager = signInManager;
             _userManager = userManager;
+            _appSettings = appSettings.Value;
         }
 
         [HttpGet]
@@ -44,7 +58,7 @@ namespace NerdStoreEnterprise.Identidade.API.Controllers
             if (result.Succeeded)
             {
                 await _signInManager.SignInAsync(user, false);
-                return  Ok();
+                return Ok(await GerarJwt(usuarioCadastro.Email));
             }
 
             return BadRequest();
@@ -60,11 +74,66 @@ namespace NerdStoreEnterprise.Identidade.API.Controllers
 
             if (result.Succeeded)
             {
-                return Ok();
-            }
+                return Ok(await GerarJwt(usuarioLogin.Email));
+                //return Ok()
+                ; }
 
             return BadRequest();
         }
 
+        private async Task<UsuarioRespostaLoginViewModel> GerarJwt(string email)
+        {
+            // buscar o email passado. Não vou me preocupar pq sei que ele existe pq ja foi autenticado
+            var user = await _userManager.FindByEmailAsync(email);
+            var claims = await _userManager.GetClaimsAsync(user);
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            // Tipos de Claim especifica para JWT
+            claims.Add(new Claim(JwtRegisteredClaimNames.Sub, user.Id));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Email, user.Email));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Nbf, ToUnixEpochDate(DateTime.UtcNow).ToString()));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Iat, ToUnixEpochDate(DateTime.UtcNow).ToString(), ClaimValueTypes.Integer64));
+
+            foreach (var userRole in userRoles)
+            {
+                claims.Add(new Claim("role", userRole));
+            }
+
+            var identityClaims = new ClaimsIdentity();
+            identityClaims.AddClaims(claims);
+
+            // Gerar o manipulador do token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+
+            var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
+            {
+                Issuer = _appSettings.Emissor,
+                Audience = _appSettings.ValidoEm,
+                Subject = identityClaims,
+                Expires = DateTime.UtcNow.AddHours(_appSettings.ExpiracaoHoras),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            });
+
+            var encodedToken = tokenHandler.WriteToken(token);
+
+            var response = new UsuarioRespostaLoginViewModel
+            {
+                AccessToken = encodedToken,
+                ExpireIn = TimeSpan.FromHours(_appSettings.ExpiracaoHoras).TotalSeconds,
+                UsuarioToken = new UsuarioTokenViewModel
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    Claims = claims.Select(c => new UsuarioClaimViewModel { Type = c.Type, Value = c.Value })
+                }
+            };
+
+            return response;
+        }
+
+        private static long ToUnixEpochDate(DateTime date)
+            => (long)Math.Round((date.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
     }
 }
---
+++ src/services/NerdStoreEnterprise.Identidade.API/Models/UsuarioRespostaLoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NerdStoreEnterprise.Identidade.API.Models
+{
+    public class UsuarioRespostaLoginViewModel
+    {
+        public string AccessToken { get; set; }
+        public double ExpireIn { get; set; }
+        public UsuarioTokenViewModel UsuarioToken { get; set; }
+    }
+}
