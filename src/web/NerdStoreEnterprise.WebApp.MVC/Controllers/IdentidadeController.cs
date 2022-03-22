2622dfdd5d9b95a5ad7d398f226a58b717c9f497
Esdras Costa <esdras.cts@outlook.com>
2022-03-22 15:37:50 -0300

    [ALT] Gerando o cookie através do JWT. Conseguimos realizar o login na aplicação.

--- src/web/NerdStoreEnterprise.WebApp.MVC/Controllers/IdentidadeController.cs
+++ src/web/NerdStoreEnterprise.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -1,6 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
 using NerdStoreEnterprise.WebApp.MVC.Models;
 using NerdStoreEnterprise.WebApp.MVC.Services;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace NerdStoreEnterprise.WebApp.MVC.Controllers
@@ -31,9 +37,11 @@ namespace NerdStoreEnterprise.WebApp.MVC.Controllers
             // Comunicar-se com a API para realizar o Registro
             var resposta = await _autenticacaoService.Registro(usuarioRegistro);
 
-            if (false) return View(usuarioRegistro);
+            //if (false) return View(usuarioRegistro);
 
             // Caso contrário, realizar o login na APP e redirecionar para algum lugar
+            await RealizarLogin(resposta);
+
             return RedirectToAction("Index", "Home");
         }
 
@@ -54,9 +62,12 @@ namespace NerdStoreEnterprise.WebApp.MVC.Controllers
             // Comunicar-se com a API para realizar o Login
             var resposta = await _autenticacaoService.Login(usuarioLogin);
 
-            if (false) return View(usuarioLogin);
+            //if (false) return View(usuarioLogin);
 
             // Caso contrário, realizar o login na APP e redirecionar para algum lugar
+            await RealizarLogin(resposta);
+
+
             return RedirectToAction("Index", "Home");
         }
 
@@ -67,6 +78,32 @@ namespace NerdStoreEnterprise.WebApp.MVC.Controllers
             return RedirectToAction("Index", "Home");
         }
 
+        private async Task RealizarLogin(UsuarioRespostaLogin usuarioResposta)
+        {
+            var token = ObterTokenFormatado(usuarioResposta.AccessToken);
 
+            var claims = new List<Claim>();
+
+            claims.Add(new Claim("JWT", usuarioResposta.AccessToken));
+            claims.AddRange(token.Claims);
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var authProperties = new AuthenticationProperties
+            {
+                ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(60),
+                IsPersistent = true
+            };
+
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity), 
+                authProperties);
+        }
+
+        private static JwtSecurityToken ObterTokenFormatado(string jwtToken)
+        {
+            return new JwtSecurityTokenHandler().ReadToken(jwtToken) as JwtSecurityToken;
+        }
     }
 }
